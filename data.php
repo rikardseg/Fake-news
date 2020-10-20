4b73d2da980ecd264656dd439542cb3934b34381
rikardseg <70521759+rikardseg@users.noreply.github.com>
2020-10-20 17:39:54 +0200

    array? XD

--- data.php
+++ data.php
@@ -6,5 +6,63 @@ declare(strict_types=1);
 // authors.
 
 $articles = [
-   
-]
+    ['title' => 'Article 1',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2013-8-19,
+    'like counter' => 20],
+
+    ['title' => 'Article 2',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2013-9-15,
+    'like counter' => 20],
+
+    ['title' => 'Article 3',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2013-9-27,
+    'like counter' => 20],
+
+    ['title' => 'Article 4',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2013-10-5,
+    'like counter' => 20],
+
+    ['title' => 'Article 5',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2013-10-22,
+    'like counter' => 20],
+
+    ['title' => 'Article 6',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2014-1-17,
+    'like counter' => 20],
+
+    ['title' => 'Article 7',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2014-3-5,
+    'like counter' => 20],
+
+    ['title' => 'Article 8',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2015-6-8,
+    'like counter' => 20],
+
+    ['title' => 'Article 9',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2015-4-13,
+    'like counter' => 20],
+
+    ['title' => 'Article 10',
+    'content' => 'blablablbal',
+    'author' => 'Ricky the licky',
+    'published date' => 2016-5-11,
+    'like counter' => 20],
+];
