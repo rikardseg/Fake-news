80eab5005b6d9e33927cbe34df2ebda015e44875
rikardseg <70521759+rikardseg@users.noreply.github.com>
2020-10-20 18:32:17 +0200

    foreach

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require __DIR__.'/data.php';
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
@@ -15,6 +16,11 @@
 </head>
 
 <body>
+
+<?php foreach($articles as $article): 
+    echo $article['title'];?>
+     <br>
+<?php endforeach;?>
 </body>
 
 </html>
